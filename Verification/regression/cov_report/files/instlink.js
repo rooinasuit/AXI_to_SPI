1de5513f736b737a248dece81826f67d031159a0
rooinasuit <roois.code@gmail.com>
2023-08-02 16:32:02 +0200

    repo language mix cleanup

--- Verification/regression/cov_report/files/instlink.js
+++
@@ -1,2 +0,0 @@
-var g_data = {"7":[6,"SPI_master0",1],"6":[5,"SPI_regs0",1],"5":[-1,"/tb_top/DUT",1]};
-processInstLinks(g_data);
--- Verification/regression/cov_report/files/sdb1.js
+++
@@ -1,2 +0,0 @@
-var g_db_data = {"6":1,"5":1};
-processScopesDbFile(g_db_data);
